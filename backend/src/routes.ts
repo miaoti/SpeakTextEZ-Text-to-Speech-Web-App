1e6460b0d98233744742322ef5a55b8a4744fd38
Jorge Yero Salazar <jyero96@gmail.com>
2023-08-01 07:57:43 -0500

    Allow uploading image

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,19 @@
 import { Application } from "express";
 import "express-async-errors";
+import multer from "multer";
+import { detectText } from "./services/ocr";
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 export const registerRoutes = (app: Application) => {
-  // TODO: add routes
+  app.post("/ocr", upload.single("image"), (req, res) => {
+    const image = req.file?.buffer;
+    if (!image) {
+      res.sendStatus(400).json({ error: "Missing image" });
+      return;
+    }
+    // TODO: Validate image?
+
+    detectText(image);
+  });
 };
